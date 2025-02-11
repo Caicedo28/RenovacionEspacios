d98aa839b24b9b0436f50e819b16e01f30eff6cb
Caicedo28 <0988612631JC@gmail.com>
2025-02-11 11:18:58 -0500

    Update settings.py

--- renovacion/settings.py
+++ renovacion/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'whitenoise.runserver_nostatic',
     'Aplicaciones.Espacios'
 ]
 
@@ -51,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'renovacion.urls'
